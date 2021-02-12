fea2d3e1bbff70e61cc05b9c055c8633010a8022
dyaksa <diasnour0395@gmail.com>
2021-02-12 13:59:48 +0700

    make own function on model Post

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -10,4 +10,21 @@ class Post extends Model
     use HasFactory;
 
     protected $table = "posts";
+
+    //make own function in model
+    public function scopeLatestPost()
+    {
+        return $this->latest()->first();
+    }
+
+    public function scopeCreatedAsc()
+    {
+        return $this->orderBy('created_at', 'asc')->get();
+    }
+
+    public function scopeCount()
+    {
+        $post = $this->all();
+        return $post->count();
+    }
 }
