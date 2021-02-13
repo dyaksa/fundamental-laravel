e195ba3f36c98221202399854c3e42c05462eef5
dyaksa <diasnour0395@gmail.com>
2021-02-13 19:19:28 +0700

    add validation rules

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -8,11 +8,21 @@
                 @csrf
                 <div class="mb-3">
                     <label for="titleInput" class="form-label">Title</label>
-                    <input type="text" class="form-control" id="titleInput" name="title" placeholder="Input Title">
+                    <input type="text" class='form-control @error("title") is-invalid @enderror' id="titleInput" name="title" placeholder="Input Title">
+                    @error('title')
+                    <div class="invalid-feedback">
+                        {{ $message }}
+                    </div>
+                    @enderror
                 </div>
                 <div class="mb-3">
                     <label for="bodyInput" class="form-label">Content</label>
-                    <textarea class="form-control" id="bodyInput" name="body"></textarea>
+                    <textarea class="form-control  @error('body') is-invalid @enderror " id="bodyInput" name="body"></textarea>
+                    @error('body')
+                    <div class="invalid-feedback">
+                        {{ $message }}
+                    </div>
+                    @enderror
                 </div>
                 <button class="btn btn-primary btn-lg">Submit</button>
             </form>
